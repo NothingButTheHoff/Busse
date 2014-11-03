73d98268b5a3568edfd0e5ccd5e1053df5a5f4e3
Per Erik <pererikfinstad@gmail.com>
2014-11-03 18:06:50 +0100

    Added rights for internet access and checks connectivity

--- src/com/pefi/Busse/MainActivity.java
+++ src/com/pefi/Busse/MainActivity.java
@@ -1,15 +1,38 @@
 package com.pefi.Busse;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    public final static String TAG = "MainActivity";
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        checkInternetConnection();
+
+    }
+
+
+    
+    public void checkInternetConnection(){
+
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Log.i(TAG, "Connected to internet");
+        } else {
+            Log.i(TAG, "Internet is not available");
+        }
+
     }
 }

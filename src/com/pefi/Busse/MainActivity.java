7b63e1dfeb1a863d833fd5501aba59654e8e6603
Per Erik <pererikfinstad@gmail.com>
2014-11-11 13:52:04 +0100

    Added a searchable activity

--- src/com/pefi/Busse/MainActivity.java
+++ src/com/pefi/Busse/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pefi.Busse;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
     private final static String TAG = "MainActivity";
@@ -22,17 +24,36 @@ public class MainActivity extends Activity {
 
     }
 
+    /**
+     * Method for creating the Action Bar menu and associates the
+     * searchable configuration with the SearchView
+     *
+     * @param menu The Options menu with your menu items
+     * @return Return true if you want the menu to be display. If not, return false
+     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
 
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
 
+    /**
+     * Method for checking the internet connection. For debugging only.
+     * The method doesn't have any return values, but instead it writes
+     * the result to the Log.
+     *
+     */
     public void checkInternetConnection(){
-
 
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
---
+++ src/com/pefi/Busse/SearchableActivity.java
@@ -0,0 +1,35 @@
+package com.pefi.Busse;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by pererikfinstad on 11/11/14.
+ */
+public class SearchableActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            doMySearch(query);
+        }
+
+    }
+
+
+    public void doMySearch(String query){
+        return;
+    }
+
+
+
+}
